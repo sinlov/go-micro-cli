e5e276a5c960d9cea0a5b168f30acb66202e7fc7
sinlov <sinlovgmppt@gmail.com>
2019-10-09 17:21:40 +0800

    fix docker tag set

--- dist/build-alpine.sh
+++ dist/build-alpine.sh
@@ -5,9 +5,9 @@ go_proxy_url=https://goproxy.io/
 
 docker_temp_contain=temp-go-micro-cli
 docker_temp_name=temp-micro/go-micro-cli
-docker_temp_tag=lastest
+docker_temp_tag=latest
 docker_cp_from=/micro
-docker_cp_to=../../lastest/alpine
+docker_cp_to=../../latest/alpine
 
 
 run_path=$(pwd)
